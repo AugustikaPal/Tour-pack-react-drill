a7a512bc74832c4179eeece9fc78ef8518ba2a1f
Augustika <augustika1524@gmail.com>
2024-01-27 15:28:17 +0530

    tour-pack

---
+++ src/data.js
@@ -0,0 +1,63 @@
+const data =[
+    {
+        id:1,
+        name:'Bali',
+        info:"Bali Island is a small beautiful island and a part of Indonesia’s archipelago, and the most famous of Indonesian tourism in the world. It owns the panorama and unique culture that makes this island exclusive to others. Furthermore, Its location is in the tropical situation as Dream Island for a vacation. Bali Island has many places of interest such as rice paddies, beautiful panorama, volcanoes, tourism activities as well as attractions. Also, it also has a beautiful jungle, long sandy beaches, warm blue water, crashing surf and friendly people. Moreover, the local people present daily community rituals and a lot of things make your holiday unforgettable. In Bali, the spirits are coming out to play in the moonlight. You can discover a festival and even a funeral on the island. Meanwhile, a good time of the day with a sea breeze will complete your holiday dream.",
+        image:'https://plus.unsplash.com/premium_photo-1664124888904-435121e89c74?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:' ₹ 45,500'
+    },
+    {
+        id:2,
+        name:'Nainital',
+        info:'Nainital, a captivating hill station situated in the Kumaon region of Uttarakhand, India, beckons travelers with its picturesque charm and serene surroundings. Nestled at an altitude of approximately 2,084 meters, the town is named after the beautiful Naini Lake that graces its landscape. Discovered by Britisher P. Barron in 1839, Nainital served as a favored summer retreat during the colonial era. The heart of the town is the pear-shaped Naini Lake, offering boating and scenic vistas, with its northern end known as Mallital and the southern one as Tallital. Visitors can revel in breathtaking views of the snow-capped Himalayas from the Snow View Point and explore the vibrant Mall Road, a cultural and shopping hub. The sacred Naina Devi Temple adds a spiritual touch to the town. Adventure enthusiasts can engage in trekking, horse riding, and boating, making the most of Nainital natural beauty. The climate is temperate, with cool summers and cold winters, attracting tourists year-round. Home to prestigious institutions like Sherwood College, Nainital is accessible through the nearby Kathgodam railway station and Pantnagar Airport. The local culture reflects a blend of Kumaoni traditions and colonial influences, offering a unique and enriching experience for those seeking both tranquility and adventure in the lap of the Himalayas.',
+        image:'https://plus.unsplash.com/premium_photo-1690372792452-6e7ee16bdeb9?q=80&w=1888&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:'₹35,000'
+    },
+   
+    {
+        id:3,
+        name:'Himachal',
+        info:`Himachal Pradesh, often referred to as the "Land of Gods," is a captivating state in the northern part of India, renowned for its diverse landscapes, vibrant culture, and adventure opportunities. Nestled in the lap of the western Himalayas, Himachal Pradesh is a haven for nature lovers and thrill-seekers alike. The picturesque valleys of Kullu and Manali, adorned with lush greenery and the Beas River, offer a serene retreat, while the snow-capped peaks of Shimla and Dharamshala beckon with their breathtaking beauty. Adventure enthusiasts are drawn to the region's offerings, from paragliding in Bir-Billing to trekking through the scenic trails of Triund and Hampta Pass. Dharamshala, home to the Tibetan government-in-exile, not only provides a spiritual refuge but also showcases stunning views of the Dhauladhar Range. The state's hill stations, including Shimla and Manali, with their colonial charm and vibrant markets, serve as ideal getaways. Himachal Pradesh is steeped in cultural heritage, featuring festivals like Kullu Dussehra and the Fagli Festival, along with traditional architecture seen in temples and old structures. Wildlife sanctuaries like the Great Himalayan National Park and the winter wonderland of Solang Valley contribute to the state's diverse appeal. `,
+        image:'https://images.unsplash.com/photo-1587293005014-ecd16293d120?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:'₹ 35,800'
+    },
+    {
+        id:4,
+        name:'Kashmir',
+        info:`Kashmir, often hailed as "Paradise on Earth," is a region of unparalleled beauty and cultural richness. Nestled in the northern part of the Indian subcontinent, this union territory beckons visitors with its breathtaking landscapes and historical significance. Srinagar, the summer capital, is renowned for the enchanting Dal Lake, where houseboats offer a unique and tranquil stay. Gulmarg, a picturesque hill station, attracts nature lovers and adventure enthusiasts alike with its meadows and skiing opportunities. The Mughal Gardens of Shalimar Bagh and Nishat Bagh showcase splendid Mughal architecture, while the Shankaracharya Temple perched on a hill provides panoramic views. Kashmir's spiritual essence is embodied in sites like the revered Hazratbal Shrine. The region's arts and handicrafts, including Pashmina shawls and Kashmiri carpets, reflect a rich cultural heritage. Betaab Valley and Pahalgam offer serene landscapes with lush greenery and snow-capped mountains. The traditional Kashmiri cuisine, especially the elaborate Wazwan, adds a delightful culinary dimension to the experience. While the region is historically and politically significant, it remains an enchanting destination for those seeking a harmonious blend of natural wonders, cultural treasures, and spiritual retreats. However, travelers are advised to stay informed about the current situation before planning their visit.`,
+        image:'https://images.unsplash.com/photo-1566323124805-757e5c41d37c?q=80&w=1887&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:'₹ 45,000'
+    },
+    {
+        id:5,
+        name:'Ladakh',
+        info:`Ladakh, often referred to as the "Land of High Passes," is a mesmerizing region in the northernmost part of India, nestled within the great Himalayas and the Karakoram Range. Renowned for its stark, breathtaking landscapes and vibrant cultural heritage, Ladakh is a destination like no other.The capital city, Leh, sits at an altitude of around 3,500 meters, surrounded by snow-capped peaks and arid mountains. Ladakh is a cold desert, with a unique topography that includes high mountain passes, deep valleys, and pristine lakes.Ladakh's Buddhist heritage is evident in its monasteries, such as Thiksey, Hemis, and Diskit. These monastic complexes, perched on hillsides, house ancient relics, murals, and serve as centers for spiritual learning and festivals. The Hemis Festival, celebrated with vibrant mask dances, is a highlight of Ladakh's cultural calendar.`,
+        image:'https://images.unsplash.com/photo-1624890240392-da0b1aa01c90?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:'₹ 55,000'
+    },
+    {
+        id:6,
+        name:'Shimla',
+        info:`Shimla, the enchanting capital of Himachal Pradesh, is a renowned hill station that captivates visitors with its colonial charm and panoramic landscapes. Nestled in the Himalayan foothills, Shimla stands at an elevation of approximately 2,276 meters above sea level. Its history dates back to the British colonial era when it served as the summer capital of British India. The Mall Road, a bustling promenade, is the heart of Shimla, lined with shops, cafes, and colonial architecture. The Ridge, a spacious open space on the Mall, offers stunning views of the surrounding mountains. One of Shimla's iconic attractions is the Christ Church, a neo-Gothic structure that adds to the town's architectural grandeur. The town is surrounded by pine and deodar forests, providing ample opportunities for nature walks and trekking. Jakhu Hill, crowned by the Hanuman Temple, offers breathtaking views of the Himalayas. `,
+        image:'https://images.unsplash.com/photo-1626621341517-bbf3d9990a23?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:'₹ 75,000'
+    },
+    {  id:7,
+        name:'Meghalaya',
+        info:`Meghalaya, a northeastern state in India, is a captivating destination known for its lush landscapes, vibrant culture, and unique natural attractions. Situated in the eastern part of the country, Meghalaya means "Abode of Clouds," a name befitting its scenic beauty.The state is characterized by its picturesque hills, dense forests, and cascading waterfalls. Shillong, the capital, is often referred to as the "Scotland of the East" for its pleasant climate and rolling hills. One of Meghalaya's highlights is the living root bridges found in the villages around Cherrapunji and Mawlynnong, crafted by intertwining the roots of rubber trees to form sturdy bridges.Meghalaya is home to the wettest place on earth, Cherrapunji, which receives heavy rainfall and boasts stunning landscapes, including Nohkalikai Falls, one of the tallest plunge waterfalls in India. Another notable attraction is the Mawsmai Caves, a network of limestone caves in the East Khasi Hills.`,
+        image:'https://images.unsplash.com/photo-1625654325562-762dcec9e6f2?q=80&w=1974&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:'₹ 65,000'
+    },
+    {  id:8,
+        name:'Kashi/Banaras',
+        info:`Banaras, also known as Varanasi, is a city steeped in history, spirituality, and cultural richness. Nestled along the banks of the sacred Ganges River, Banaras is one of the oldest continuously inhabited cities in the world. Its labyrinthine streets are adorned with vibrant ghats, ancient temples, and bustling markets, creating an immersive experience for visitors.
+        Renowned for its spiritual significance, Banaras is a pilgrimage site for Hindus who seek salvation by bathing in the Ganges. The city's spiritual aura is complemented by its rich art and music scene, with classical performances and traditional crafts flourishing in its narrow lanes.
+        Exploring Banaras reveals a tapestry of rituals, from the mesmerizing Ganga Aarti at Dashashwamedh Ghat to the lively ghats lined with devotees and sadhus. The city's unique blend of tradition and modernity, coupled with its timeless charm, makes Banaras an enchanting destination that captivates the soul and leaves an indelible mark on all who visit.`,
+        image:'https://images.unsplash.com/photo-1561359313-0639aad49ca6?q=80&w=1935&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+        price:'₹ 33,000'
+    }
+
+
+];
+
+export default data;
